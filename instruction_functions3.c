ff557dbbb4b8e2da0bd00cc6ef6e77bac866feb6
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-10-19 22:55:10 +0300

    [fix] project monty tasks[12]

--- instruction_functions3.c
+++ instruction_functions3.c
@@ -66,6 +66,7 @@ void pstr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current;
 
+	(void) line_number;
 	current = *stack;
 	while (current != NULL && current->n > 0 && current->n <= 127)
 	{
